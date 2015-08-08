c957d9893e72bfbe863f58e221e838538bd7beaf
GoshaFighten <goshafighten@gmail.com>
2015-08-08 23:17:17 +0300

    FileView added

---
+++ FolderExplorer/FolderExplorer/ViewModels/FileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm;
+
+namespace FolderExplorer.ViewModels {
+    [POCOViewModel]
+    public class FileViewModel : ISupportParameter {
+        public string Path { get; set; }
+        public object Parameter {
+            get { throw new NotImplementedException(); }
+            set { Path = (string)value; }
+        }
+    }
+}
---
+++ FolderExplorer/FolderExplorer/Views/FileView.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FolderExplorer.Views
+{
+    partial class FileView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mvvmContext1 = new DevExpress.Utils.MVVM.MVVMContext(this.components);
+            this.richEditControl1 = new DevExpress.XtraRichEdit.RichEditControl();
+            ((System.ComponentModel.ISupportInitialize)(this.mvvmContext1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mvvmContext1
+            // 
+            this.mvvmContext1.ContainerControl = this;
+            this.mvvmContext1.ViewModelType = typeof(FolderExplorer.ViewModels.FileViewModel);
+            // 
+            // richEditControl1
+            // 
+            this.richEditControl1.ActiveViewType = DevExpress.XtraRichEdit.RichEditViewType.Simple;
+            this.richEditControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richEditControl1.EnableToolTips = true;
+            this.richEditControl1.Location = new System.Drawing.Point(0, 0);
+            this.richEditControl1.Name = "richEditControl1";
+            this.richEditControl1.Options.Bookmarks.AllowNameResolution = false;
+            this.richEditControl1.ReadOnly = true;
+            this.richEditControl1.Size = new System.Drawing.Size(512, 314);
+            this.richEditControl1.TabIndex = 0;
+            this.richEditControl1.Text = "richEditControl1";
+            // 
+            // FileView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.richEditControl1);
+            this.Name = "FileView";
+            this.Size = new System.Drawing.Size(512, 314);
+            this.Load += new System.EventHandler(this.FileView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mvvmContext1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private DevExpress.Utils.MVVM.MVVMContext mvvmContext1;
+        private DevExpress.XtraRichEdit.RichEditControl richEditControl1;
+    }
+}
